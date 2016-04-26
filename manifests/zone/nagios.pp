87d30da7ea52b9266695fb4f0ba46e9023c240e9
b4ldr <github@johnbond.org>
2016-04-26 15:17:23 +0100

    rename variables

--- manifests/zone/nagios.pp
+++ manifests/zone/nagios.pp
@@ -9,23 +9,23 @@ define nsd::zone::nagios (
   $_masters = delete($masters,['127.0.0.1','0::1'])
   $_slaves  = delete($slaves,['127.0.0.1','0::1'])
   if $_masters {
-    $check_args = join($_masters, ' ')
+    $master_check_args = join($_masters, ' ')
     @@nagios_service{ "${::fqdn}_DNS_ZONE_MASTERS_${name}":
       ensure              => present,
       use                 => 'generic-service',
       host_name           => $::fqdn,
       service_description => "DNS_ZONE_MASTERS_${name}",
-      check_command       => "check_nrpe_args!check_dns!${name}!${check_args}",
+      check_command       => "check_nrpe_args!check_dns!${name}!${master_check_args}",
     }
   }
   if $_slaves {
-    $check_args = join($_slaves, ' ')
+    $slave_check_args = join($_slaves, ' ')
     @@nagios_service{ "${::fqdn}_DNS_ZONE_SLAVES_${name}":
       ensure              => present,
       use                 => 'generic-service',
       host_name           => $::fqdn,
       service_description => "DNS_ZONE_MASTERS_${name}",
-      check_command       => "check_nrpe_args!check_dns!${name}!${check_args}",
+      check_command       => "check_nrpe_args!check_dns!${name}!${slave_check_args}",
     }
   }
 
