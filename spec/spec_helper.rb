6be3470b5635f06fe2956c85f63fbe704141d732
b4ldr <github@johnbond.org>
2016-07-08 12:55:06 +0100

    update

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,14 +5,8 @@ require 'hiera-puppet-helper'
 require 'rspec-puppet-facts'
 include RspecPuppetFacts
 require 'puppet-lint/tasks/puppet-lint'
-
-Rake::Task[:lint].clear
-
-PuppetLint::RakeTask.new :lint do |config|
-  config.ignore_paths = ["spec/**/*.pp", "vendor/**/*.pp"]
-  config.log_format = '%{path}:%{linenumber}:%{KIND}: %{message}'
-  config.disable_checks = [ "disable_class_inherits_from_params_class"]
-end
+PuppetLint.configuration.ignore_paths = ["vendor/**/*.pp", "spec/**/*.pp"]
+PuppetLint.configuration.send("disable_class_inherits_from_params_class")
 
 # Uncomment this to show coverage report, also useful for debugging
 at_exit { RSpec::Puppet::Coverage.report! }
