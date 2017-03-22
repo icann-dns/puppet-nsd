dddd34265f10541aa680f6011be51ef0e3cc980d
b4ldr <github@johnbond.org>
2017-03-22 19:18:56 +0000

    make key name optional

--- manifests/tsig.pp
+++ manifests/tsig.pp
@@ -1,10 +1,10 @@
 # Define: nsd::tsig
 #
 define nsd::tsig (
-    String    $data,
-    Nsd::Algo $algo     = 'hmac-sha256',
-    String    $template = 'nsd/etc/nsd/nsd.key.conf.erb',
-    String    $key_name = undef,
+    String           $data,
+    Nsd::Algo        $algo     = 'hmac-sha256',
+    String           $template = 'nsd/etc/nsd/nsd.key.conf.erb',
+    Optional[String] $key_name = undef,
 ) {
   include ::nsd
   concat::fragment{ "nsd_key_${name}":
