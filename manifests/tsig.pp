868a48cf3b8a630c566158c0bb0034dbf6f754cc
b4ldr <github@johnbond.org>
2017-03-22 19:00:19 +0000

    add abbility to overrid key name

--- manifests/tsig.pp
+++ manifests/tsig.pp
@@ -4,6 +4,7 @@ define nsd::tsig (
     String    $data,
     Nsd::Algo $algo     = 'hmac-sha256',
     String    $template = 'nsd/etc/nsd/nsd.key.conf.erb',
+    String    $key_name = undef,
 ) {
   include ::nsd
   concat::fragment{ "nsd_key_${name}":
